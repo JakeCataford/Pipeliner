9ccaa343325384eb8c41458c1c20485105459464
JakeCataford <JakeCataford@Gmail.com>
2013-07-26 09:59:45 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pipeliner::Application.config.secret_key_base = 'd132a0b2ec87891b39efe06b81be5ad46c9fd6464ed455f9aa05defe5766d0a122b661c8bf684ac0bfe9c05e0e6eff7ca9c54d242d8115dff724f5dfa3ceb5cd'
